5cd1705ed3bfdb54bbaa643f2bb2f8bae31cbf71
MohammedYasin26 <106878921+MohammedYasin26@users.noreply.github.com>
2022-07-15 01:15:16 +0530

    Add files via upload

---
+++ Divisibleby.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"enter a number :"<<endl;
+    cin>>n;
+
+    if (n%5==0)
+    {
+        cout<<"The given number is divisible by 5";
+    }
+    else
+    {
+        cout<<"The given number is non divisible by 5";
+    }
+    
+}
