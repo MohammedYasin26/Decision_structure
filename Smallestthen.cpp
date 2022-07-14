4bfdab1c77126e3201bdf3fa36f03c1f89a86a00
MohammedYasin26 <106878921+MohammedYasin26@users.noreply.github.com>
2022-07-15 01:14:13 +0530

    Add files via upload

---
+++ Smallestthen.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+   
+     
+    int a,b;
+
+    cout<<"Enter the value of A :"<<endl;
+    cin>>a;
+
+    cout<<"Enter the value of B :"<<endl;
+    cin>>b;
+
+    if(a<b)
+    {
+        cout<<"A is smallest then B "<<endl;
+    }
+    else
+    {
+        cout<<"B is smallest then A "<<endl;
+    }
+
+    return 0;
+}
